d0c90fb239265791cf3ce18d3f39f70f0734bd82
William Zhang <willzhang05@gmail.com>
2019-10-09 23:59:00 -0400

    solution for k

--- 19f/prac5/klep.py
+++ 19f/prac5/klep.py
@@ -5,35 +5,20 @@ def main():
     n = int(nums[0])
     m = int(nums[1])
     seen = input()
-    #seen_codes = list(map(lambda x: ord(x) - 97, seen))
     ctext = input()
     ctext_codes = list(map(lambda x: ord(x) - 97, ctext))
-    #find key first
-    decrypt = seen
-    i = n - 1
-    j = m - n
-    while len(decrypt) < m:
-        a = ord(decrypt[0]) - 97
-        b = ctext_codes[j]
-        #print("a", chr(97 + a))
-        #print("b", chr(97 + b))
-        p = 0
-        value = 0
-        possible = list()
-        while True:
-            value = b+26*p-a
-            if value >= 26:
-                break
-            if value < 26 and value >= 0:
-               possible.append(chr(97 + value))
-               decrypt = ''.join((str(chr(97 + value)),decrypt))
-            p += 1
-
-        print(possible)
-        #print(list(map(lambda x: ord(x) - 97, possible)))
+    decrypt = [' ']*m
+    for i in range(n):
+        decrypt[m - n + i] = seen[i];
+    i = m - n - 1
+    while i >= 0:
+        b = ctext_codes[i + n]
+        a = ord(decrypt[i + n]) - 97
+        k = (b-a + 26) % 26
+        decrypt[i] = chr(k + 97)
         i -= 1
-        j -= 1
-    print(decrypt)
+
+    print(''.join(decrypt))
         
 
 if __name__ == "__main__":
